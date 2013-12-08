87a05570e06a325ff78beeba8eddf16c028c25ca
Joel Sonderegger <json@bluewin.ch>
2013-12-08 21:43:43 +0000

    changed web.js reading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
+var fs = require('fs');
+
+var data = fs.readFileSync("index.html", "utf-8");
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(data));
 });
 
 var port = process.env.PORT || 5000;
